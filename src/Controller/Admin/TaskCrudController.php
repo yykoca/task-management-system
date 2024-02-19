1a881e96d0d5e20f85b43364e78948ad52d23df3
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-02-19 17:38:04 +0100

    Add admin dashboard and add crud links for user and task entities

---
+++ src/Controller/Admin/TaskCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Task;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class TaskCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Task::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
