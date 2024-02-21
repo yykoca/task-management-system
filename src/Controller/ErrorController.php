4ddf5e5a98c82be036cd570a83e25326c0785eb9
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-02-21 12:45:52 +0100

    Add exception message in to the error template

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -4,11 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Throwable;
 
 class ErrorController extends AbstractController
 {
-    public function show(): Response
+    public function show(Throwable $exception): Response
     {
-        return $this->render('error.html.twig');
+        if ($exception instanceof HttpExceptionInterface && $exception->getStatusCode() === 403) {
+            return $this->render('access_denied.html.twig', ['error' => $exception]);
+        }
+
+        return $this->render('error.html.twig', ['exception' => $exception]);
     }
 }
